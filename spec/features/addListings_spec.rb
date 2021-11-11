fb109236ee10ef8db08ace571754552088ffd5ab
Yassaha Ali <yassahaali@gmail.com>
2021-11-11 15:27:31 +0000

    fixed failing test - user passes information add

--- spec/features/addListings_spec.rb
+++ spec/features/addListings_spec.rb
@@ -1,7 +1,6 @@
 feature 'user passes information add a new listing' do 
     scenario 'user adds listing' do 
       visit('/addListings')
-      fill_in 'user_id', with: 'Yas'
       fill_in 'description', with: 'looks good'
       fill_in 'location', with: 'location'
       fill_in 'start_date', with: '07/09/2021'

c8fde659f8548b1e75bde63dfd9aff2a40fa74f6
Yassaha Ali <yassahaali@gmail.com>
2021-11-12 11:42:41 +0000

    fixed form and failign tests

--- spec/features/addListings_spec.rb
+++ spec/features/addListings_spec.rb
@@ -3,8 +3,8 @@ feature 'user passes information add a new listing' do
       visit('/addListings')
       fill_in 'description', with: 'looks good'
       fill_in 'location', with: 'location'
-      fill_in 'start_date', with: '07/09/2021'
-      fill_in 'end_date', with: '09/09/2021'
+      fill_in 'start_date', with: '07-09-2021'
+      fill_in 'end_date', with: '09-09-2021'
       fill_in 'price', with: '1000'
       click_button 'Upload'
       expect(page).to have_content 'looks good'
